94e73492d26a0fb0702dd0101bd503dfcb571a84
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2021-09-05 18:12:06 +0800

    added base installation script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+##!/bin/bash
+
+# check for better mirrors
+sudo pacman --debug -Sy --noconfirm
+
+# install development tools
+sudo pacman -S --noconfirm devel-tools
+sudo pacman -S --noconfirm git
+
+# install yay
+cd $HOME
+sudo git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+sudo mv yay /opt
+
+sudo pacman -S --noconfirm sway swayidle swaylock grim waybar wofi pactl pavucontrol slurp wl-clipboard jq xdg-desktop-portal-wlr libpipewire02 zenity mako lsd noto-fonts-emoji bluez bluez-utils blueman awesome-terminal-fonts openssh stow
+
+# yay -S autotiling
+yay -S seatd
+
