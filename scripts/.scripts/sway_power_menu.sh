1fb53762b46f3c1ccafa890bddf226bd589511f9
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2021-10-02 19:06:06 +0800

    added clear before shutdown

--- scripts/.scripts/sway_power_menu.sh
+++ scripts/.scripts/sway_power_menu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-swaynag -t power -m '' -b ' Logout' 'swaymsg exit' -b '⏾ Sleep' 'systemctl suspend' -b '⟳ Reboot' 'systemctl reboot' -b ' Shutdown' 'systemctl poweroff' -s 'Close'
+swaynag -t power -m '' -b ' Logout' 'swaymsg exit' -b '⏾ Sleep' 'clear; systemctl suspend' -b '⟳ Reboot' 'clear; systemctl reboot' -b ' Shutdown' 'clear; systemctl poweroff' -s 'Close'
 
 
