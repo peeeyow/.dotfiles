f0fe7ed6d1f8580f45ec1e899e411a5b756f6ee3
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-01-04 14:42:30 +0800

    removed unnecessary comments

--- nvim/.config/nvim/lua/user/opts.lua
+++ nvim/.config/nvim/lua/user/opts.lua
@@ -1,71 +1,36 @@
 local options = {
-    -- creates a backup file
     backup = false,
-    -- allows neovim to access the system clipboard
     clipboard = "unnamedplus",
-    -- more space in the neovim command line for displaying messages
     cmdheight = 2,
-    -- mostly just for cmp
     completeopt = { "menu", "menuone", "noselect" },
-    -- so that `` is visible in markdown files
     conceallevel = 0,
-    -- the encoding written to a file
     fileencoding = "utf-8",
-    -- highlight all matches on previous search pattern
     hlsearch = true,
-    -- ignore case in search patterns
     ignorecase = true,
-     -- allow the mouse to be used in neovim
     mouse = "a",
-    -- pop up menu height
     pumheight = 10,
-    -- we don't need to see things like -- INSERT -- anymore
     showmode = false,
-    -- always show tabs
     showtabline = 2,
-    -- smart case
     smartcase = true,
-    -- make indenting smarter again
     smartindent = true,
-    -- force all horizontal splits to go below current window
     splitbelow = true,
-    -- force all vertical splits to go to the right of current window
     splitright = true,
-    -- creates a swapfile
     swapfile = false,
-    -- set term gui colors (most terminals support this)
     termguicolors = true,
-    -- time to wait for a mapped sequence to complete (in milliseconds)
     timeoutlen = 1000,
-    -- enable persistent undo
     undofile = true,
-    -- faster completion (4000ms default)
     updatetime = 300,
-    -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
     writebackup = false,
-    -- convert tabs to spaces
     expandtab = true,
-    -- the number of spaces inserted for each indentation
     shiftwidth = 2,
-     -- insert 2 spaces for a tab
     tabstop = 2,
-    -- highlight the current line
     cursorline = true,
-    -- set numbered lines
     number = true,
-    -- set relative numbered lines
     relativenumber = true,
-    -- set number column width to 2 {default 4}
     numberwidth = 2,
-    -- always show the sign column, otherwise it would shift the text each time
     signcolumn = "yes",
-    -- display lines as one long line
     wrap = true,
-    -- is one of my fav
-    scrolloff = 4,
-
-    sidescrolloff = 8,
-    -- the font used in graphical neovim applications
+    scrolloff = 8,
     guifont = "monospace:h17",
 }
 
@@ -90,8 +55,15 @@ vim.api.nvim_exec([[
 vim.opt.shortmess:append "c"
 
 -- don't auto comment new line
-vim.api.nvim_command([[ autocmd BufNewFile,BufRead * setlocal formatoptions-=cro ]])
--- vim.opt.formatoptions:remove("cro")
+--[=[
+vim.api.nvim_exec([[
+  augroup no_auto_comment
+      autocmd!
+      au BufNewFile,BufRead * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+  augroup END
+]], false)
+--]=]
+vim.opt.formatoptions:append { c = false, r = false, o = false }
 
 -- highlight when yanking
 vim.api.nvim_exec([[
@@ -100,13 +72,6 @@ vim.api.nvim_exec([[
         au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
     augroup END
 ]], false)
-
-
--- CACHE_PATH = vim.fn.stdpath "cache"
--- -- add python provider
--- vim.api.nvim_exec( [[
---     let g:python3_host_prog = expand("~/.config/nvim/provider_python/bin/python")
--- ]], false)
 
 -- -- add node provider
 -- --[=[
