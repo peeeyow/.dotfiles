bb99bc3a56c403929c502623b8999f3942b73b9e
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-01-15 13:45:52 +0800

    changed colorscheme to onedark

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,8 @@
 local colorscheme = "tokyonight"
 
-vim.g.tokyonight = "night"
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_italic_variables = true
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
