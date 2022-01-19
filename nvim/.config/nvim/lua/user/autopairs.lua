8f9a62a080738869bc0e6310569e1b027f640fe8
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-01-19 15:54:57 +0800

    feat(autopairs): added tex support

--- nvim/.config/nvim/lua/user/autopairs.lua
+++ nvim/.config/nvim/lua/user/autopairs.lua
@@ -1,17 +1,59 @@
-local status_ok, npairs = pcall(require, "nvim-autopairs")
+local status_ok, autopairs = pcall(require, "nvim-autopairs")
+local Rule = require "nvim-autopairs.rule"
+local cond = require "nvim-autopairs.conds"
 
 if not status_ok then
   return
 end
 
-npairs.setup {
-  check_ts = true,
-  ts_config = {
-    lua = { "string", "source" },
-    javascript = { "string", "template_string" },
-  },
-  disable_filetype = { "TelescopePrompt", "spectre_panel" },
+autopairs.setup {
+    active = true,
+    on_config_done = nil,
+    ---@usage  modifies the function or method delimiter by filetypes
+    map_char = {
+      all = "(",
+      tex = "{",
+    },
+    ---@usage check bracket in same line
+    enable_check_bracket_line = false,
+    ---@usage check treesitter
+    check_ts = true,
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+      java = false,
+    },
+    disable_filetype = { "TelescopePrompt", "spectre_panel" },
+    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+    enable_moveright = true,
+    disable_in_macro = false,
+    enable_afterquote = true,
+    map_bs = true,
+    map_c_w = false,
+    disable_in_visualblock = false,
 }
+
+
+require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
+
+autopairs.add_rule(Rule("$$", "$$", "tex"))
+  autopairs.add_rules {
+    Rule("$", "$", { "tex", "latex" }) -- don't add a pair if the next character is %
+      :with_pair(cond.not_after_regex_check "%%") -- don't add a pair if  the previous character is xxx
+      :with_pair(cond.not_before_regex_check("xxx", 3)) -- don't move right when repeat character
+      :with_move(cond.none()) -- don't delete if the next character is xx
+      :with_del(cond.not_after_regex_check "xx") -- disable  add newline when press <cr>
+      :with_cr(cond.none()),
+  }
+  autopairs.add_rules {
+    Rule("$$", "$$", "tex"):with_pair(function(opts)
+      print(vim.inspect(opts))
+      if opts.line == "aa $$" then
+        -- don't add pair on that line
+        return false
+      end
+    end),
+  }
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 local cmp_status_ok, cmp = pcall(require, "cmp")
@@ -19,4 +61,3 @@ if not cmp_status_ok then
   return
 end
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
-
