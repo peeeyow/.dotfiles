9dc192a902723a2899a3ae6755eb570bf2382f79
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-01-21 22:19:31 +0800

    fix(nvim-tree): tree now opens properly
    Fixed the bug that causes nvim tree not to open if buffer is new
    (not existing on disk).

--- nvim/.config/nvim/lua/user/tree-toggle.lua
+++ nvim/.config/nvim/lua/user/tree-toggle.lua
@@ -12,11 +12,15 @@ end
 local tree ={}
 tree.toggle = function ()
   if nvim_tree_view.win_open() then
-   bufferline_state.set_offset(0)
-   nvim_tree.close()
+    bufferline_state.set_offset(0)
+    nvim_tree.close()
   else
-   bufferline_state.set_offset(nvim_tree_view.View.width + 1, "NvimTree")
-   nvim_tree.find_file(true)
+    bufferline_state.set_offset(nvim_tree_view.View.width + 1, "NvimTree")
+    if vim.api.nvim_buf_get_name(0) == '' then
+      nvim_tree.open()
+    else
+      nvim_tree.find_file(true)
+    end
   end
 end
 
