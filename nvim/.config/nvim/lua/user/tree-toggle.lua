bf7c8ab56244a4dbfb7fb65436710a3987b8c028
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-01-22 20:37:27 +0800

    fix(tree-toggle): removed barbar dependency

--- nvim/.config/nvim/lua/user/tree-toggle.lua
+++ nvim/.config/nvim/lua/user/tree-toggle.lua
@@ -4,18 +4,18 @@ if not nvim_tree_status_ok then
 end
 local nvim_tree_view = require "nvim-tree.view"
 
-local barbar_status_ok, bufferline_state = pcall(require, "bufferline.state")
-if not barbar_status_ok then
-  return
-end
+-- local barbar_status_ok, bufferline_state = pcall(require, "bufferline.state")
+-- if not barbar_status_ok then
+--   return
+-- end
 
 local tree ={}
 tree.toggle = function ()
   if nvim_tree_view.win_open() then
-    bufferline_state.set_offset(0)
+    -- bufferline_state.set_offset(0)
     nvim_tree.close()
   else
-    bufferline_state.set_offset(nvim_tree_view.View.width + 1, "NvimTree")
+    -- bufferline_state.set_offset(nvim_tree_view.View.width + 1, "NvimTree")
     if vim.api.nvim_buf_get_name(0) == '' then
       nvim_tree.open()
     else
