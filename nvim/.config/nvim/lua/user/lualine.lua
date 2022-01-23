dc543507f10e981fe28aac778a314f8def989511
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-01-23 19:42:58 +0800

    refactor(lualine): removed unnecessary comments

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -115,7 +115,6 @@ lualine.setup {
     lualine_a = { "mode" },
     lualine_b = { branch, diff, diagnostics },
     lualine_c = {},
-    -- lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_x = { python_env, "encoding", filetype },
     lualine_y = { "progress" },
     lualine_z = { location },
