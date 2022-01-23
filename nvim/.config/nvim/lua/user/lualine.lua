11490c0ea08799838ef195d062bfc7b61f44b79b
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-01-23 22:58:38 +0800

    feat(lualine): change component separator

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -106,7 +106,7 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = "|",
+    component_separators = { left = "❭", right = "❬" },
     section_separators = { left = "", right = "" },
     disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
     always_divide_middle = true,
