3dd4c51653770b171f3ec3580c38480530b6aa53
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-02-05 21:48:27 +0800

    feat(zsh): added aliases

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,21 @@
+# alias usable for sudo
+alias sudo="sudo "
+
+# interactive
+alias rm="rm -I"
+alias cp="cp -i"
+alias mv="mv -i"
+
+# ls
+alias ls="lsd "
+alias la="ls -la"
+alias lA="ls -lA"
+
+# readable
+alias du="du -h"
+alias df="df -h"
+alias free="free -h"
+
+# programs
+alias v="nvim "
+alias r="ranger "
