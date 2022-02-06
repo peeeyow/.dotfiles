65c4b9a112c346010719676f9489049e703ddb65
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-02-06 19:20:45 +0800

    fix(zsh): call compinit before configuration

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -1,4 +1,4 @@
-autoload -Uz compinit
+autoload -Uz compinit; compinit
 zstyle ':completion:*' menu select
 
 # allow dot completion
@@ -60,5 +60,3 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 zstyle ':completion:*' keep-prefix true
 
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
-
-compinit
