9f2058a76e94da8b873648ea6c3b2450238ecd70
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-03-28 14:32:47 +0800

    feat(neovim): added clangd support

--- nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -40,6 +40,14 @@ lsp_installer.on_server_ready(function(server)
   if server.anme == "emmet_ls" then
     opts.capabilities.textDocument.completion.completionItem.snippetSupport = true
   end
+
+  if server.name == "clangd" then
+    -- HACK: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
+    opts.capabilities.offsetEncoding = { "utf-16" }
+
+    local clangd_opts = require "user.lsp.settings.clangd"
+    opts = vim.tbl_deep_extend("force", clangd_opts, opts)
+  end
   -- This setup() function is exactly the same as lspconfig's setup function.
   -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
   server:setup(opts)
