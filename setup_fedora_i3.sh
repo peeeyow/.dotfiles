bb1ce52985edd38057a2e36175b54873de8e2bb7
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-04-02 11:34:08 +0800

    feat(install.sh): added tmux setup

--- setup_fedora_i3.sh
+++ setup_fedora_i3.sh
@@ -1,6 +1,14 @@
-sudo dnf -y i3-gaps xinput arc-theme zsh polybar stow ranger arc-theme ripgrep zoxide cargo kitty dunst picom rofi feh chsh tlp tlp-rdw brightnessctl python-devel @development-tools scrot
+sudo dnf -y install i3-gaps xinput arc-theme zsh polybar stow ranger arc-theme ripgrep zoxide cargo kitty dunst picom rofi feh chsh tlp tlp-rdw brightnessctl python-devel @development-tools scrot util-linux-user lsd mozilla-fira-mono-font fontawesome5-fonts-all
+
+sudo dnf -y groupinstall "X Software Development"
 
 chsh -s `which zsh`
 
 cargo install fnm stylua
 pip install -U wheel ueberzug pynvim
+
+mkdir ~/.config/tmux/plugins/
+
+git clone https://github.com/tmux-plugins/tpm.git
+
+
