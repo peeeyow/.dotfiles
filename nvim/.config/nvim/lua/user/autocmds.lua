04f00c4766967a70e17b720830cd208811180b7d
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-05-01 13:49:01 +0800

    fix(autocommands): change formatopt config
    No longer comments after doing 'o' or 'O' in a comment line.

--- nvim/.config/nvim/lua/user/autocmds.lua
+++ nvim/.config/nvim/lua/user/autocmds.lua
@@ -2,17 +2,17 @@ local au = vim.api.nvim_create_autocmd
 local aug = vim.api.nvim_create_augroup
 
 au("TextYankPost", {
-  group = aug("highlight_yank", {}),
+  group = aug("highlight_yank", { clear = true }),
   desc = "Highlight text on yank.",
   callback = function()
     vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
   end
 })
 
---[[ au("BufEnter", {
-  group = aug("no_comment_on_enter", {}),
+au("BufEnter", {
+  group = aug("no_comment_on_enter", { clear = true }),
   desc = "No longer comments upon newline",
   callback = function()
     vim.opt_local.formatoptions:remove { "c", "r", "o" }
   end
-}) ]]
+})
