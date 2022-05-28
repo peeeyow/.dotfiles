fba87a6713478fd1656b49d1f298b5c61a1f13bc
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-05-28 21:39:47 +0800

    refactor(jsformat): Change to prettier

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -24,7 +24,7 @@ local codeactions = null_ls.builtins.code_actions
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettierd.with {
+    formatting.prettier.with {
       prefer_local = "node_modules/.bin",
     },
     formatting.black,
