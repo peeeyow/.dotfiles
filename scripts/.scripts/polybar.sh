5ae81d67adfae7f37752130b92d14f0d6c394d48
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-07-04 16:06:39 +0800

    fix(multi-monitor): Open monitor at start up

--- scripts/.scripts/polybar.sh
+++ scripts/.scripts/polybar.sh
@@ -7,4 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch mainbar
-polybar main 2>&1 | tee -a /tmp/main.log & disown
+# polybar main 2>&1 | tee -a /tmp/main.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
