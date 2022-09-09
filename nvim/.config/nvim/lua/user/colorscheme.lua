5008914dd7108f9022db7b2fffd90d0e8fb8b8ed
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2022-09-09 17:20:20 +0800

    Update tokyonight colorscheme to new API

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,8 +1,5 @@
+require "user.tokyonight"
 local colorscheme = "tokyonight"
-
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_variables = true
 
 vim.cmd [[
   " gray
---
+++ nvim/.config/nvim/lua/user/tokyonight.lua
@@ -0,0 +1,9 @@
+local status_ok, tokyonight = pcall(require, "tokyonight")
+if not status_ok then
+  return
+end
+
+tokyonight.setup {
+  style = "night",
+  dim_inactive = true,
+}
