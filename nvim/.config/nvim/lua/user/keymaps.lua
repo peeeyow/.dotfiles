9fd29494f727ba4cc9c97f804e08c47ec277995d
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2022-09-09 17:46:55 +0800

    chore(nvim): Remove comments

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,13 +1,3 @@
--- local function map(mode, lhs, rhs, opts)
---   local options = { noremap = true, silent = true }
---   -- extend options with opts having higher priority on conflict
---   if opts then
---     options = vim.tbl_extend("force", options, opts)
---   end
---   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
--- end
-
--- empty opts
 local opts = { noremap = true, silent = true }
 
 -- set leader key
