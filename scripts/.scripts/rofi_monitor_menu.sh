6dc7644e0881ed33a99568d44df49a0227bd4319
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2022-09-18 11:58:20 +0800

    feat(rofi): Configure rofi triple monitor menu

--- scripts/.scripts/rofi_monitor_menu.sh
+++ scripts/.scripts/rofi_monitor_menu.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-chosen=$(printf "External\nBuiltin\nDual" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+options="Dual_External\nDual_with_Vertical\nDual_with_Horizontal\nTriple_Full\nSingle_External\nSingle_Builtin"
+
+chosen=$(printf $options | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
-	"External") monitor_setup.sh external ;;
-	"Builtin") monitor_setup.sh builtin ;;
-	"Dual") monitor_setup.sh dual ;;
+  "Dual_External")        monitor_setup.sh dual_external ;;
+  "Dual_with_Vertical")   monitor_setup.sh dual_with_vertical ;;
+  "Dual_with_Horizontal") monitor_setup.sh dual_with_horizontal ;;
+  "Triple_Full")          monitor_setup.sh triple_full ;;
+  "Single_External")      monitor_setup.sh single_external ;;
+  "Single_Builtin")       monitor_setup.sh single_builtin ;;
 	*) exit 1 ;;
 esac
