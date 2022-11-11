80cd269f33ff4032ed3a0a877c6c4708eb0d6d12
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2022-11-11 14:25:05 +0800

    fix(nvim): Configure auto pairs for prisma

--- nvim/.config/nvim/lua/user/autopairs.lua
+++ nvim/.config/nvim/lua/user/autopairs.lua
@@ -7,16 +7,7 @@ if not status_ok then
 end
 
 autopairs.setup {
-  active = true,
-  on_config_done = nil,
-  ---@usage  modifies the function or method delimiter by filetypes
-  map_char = {
-    all = "(",
-    tex = "{",
-  },
-  ---@usage check bracket in same line
   enable_check_bracket_line = false,
-  ---@usage check treesitter
   check_ts = true,
   ts_config = {
     lua = { "string", "source" },
@@ -25,24 +16,16 @@ autopairs.setup {
   },
   disable_filetype = { "TelescopePrompt", "spectre_panel" },
   ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
-  enable_moveright = true,
-  disable_in_macro = false,
-  enable_afterquote = true,
-  map_bs = true,
-  map_c_w = false,
-  disable_in_visualblock = false,
 }
-
-require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
 
 autopairs.add_rule(Rule("$$", "$$", "tex"))
 autopairs.add_rules {
-  Rule("$", "$", { "tex", "latex" })-- don't add a pair if the next character is %
-      :with_pair(cond.not_after_regex_check "%%")-- don't add a pair if  the previous character is xxx
-      :with_pair(cond.not_before_regex_check("xxx", 3))-- don't move right when repeat character
-      :with_move(cond.none())-- don't delete if the next character is xx
-      :with_del(cond.not_after_regex_check "xx")-- disable  add newline when press <cr>
-      :with_cr(cond.none()),
+  Rule("$", "$", { "tex", "latex" })
+    :with_pair(cond.not_after_regex_check "%%")
+    :with_pair(cond.not_before_regex_check("xxx", 3))
+    :with_move(cond.none())
+    :with_del(cond.not_after_regex_check "xx")
+    :with_cr(cond.none()),
 }
 autopairs.add_rules {
   Rule("$$", "$$", "tex"):with_pair(function(opts)
@@ -55,8 +38,36 @@ autopairs.add_rules {
 }
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local handlers = require "nvim-autopairs.completion.handlers"
+
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
   return
 end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+
+cmp.event:on(
+  "confirm_done",
+  cmp_autopairs.on_confirm_done {
+    filetypes = {
+      ["*"] = {
+        ["("] = {
+          kind = {
+            cmp.lsp.CompletionItemKind.Function,
+            cmp.lsp.CompletionItemKind.Method,
+          },
+          handler = handlers["*"],
+        },
+      },
+      tex = {
+        ["{"] = {
+          kind = {
+            cmp.lsp.CompletionItemKind.Function,
+            cmp.lsp.CompletionItemKind.Method,
+          },
+          handler = handlers["*"],
+        },
+      },
+      prisma = false,
+    },
+  }
+)
