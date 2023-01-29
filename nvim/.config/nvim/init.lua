a80fe45c4793c6fc755c9b481a8555e72fcea1a2
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-01-29 15:00:12 +0800

    fix(nvim-matchit): Use for opening and ending tags cycling

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,3 @@
--- require "user.options"
--- require "user.keymaps"
--- require "user.plugins"
--- require "user.colorscheme"
--- require "user.lsp"
--- require "user.autocmds"
-
 require "config.options"
 require "config.lazy"
 
