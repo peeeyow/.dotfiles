1bec2c71e91feed720bf41954ffcf52c40ff9523
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-04-23 15:07:26 +0800

    feat(nvim-ccc): Configure color picker

---
+++ nvim/.config/nvim/lua/plugins/ccc.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "uga-rosa/ccc.nvim",
+    event = "BufReadPost",
+    opts = {
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    },
+  },
+}
