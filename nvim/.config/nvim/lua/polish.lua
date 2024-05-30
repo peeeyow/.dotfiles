daa537fe7e65b9386a5502a3753fdcf0f3c66a58
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-05-30 19:22:53 +0800

    feat(nvim): configure autocommands

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -1,18 +1,17 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+local create_autocmd = vim.api.nvim_create_autocmd
+local create_autogroup = vim.api.nvim_create_augroup
 
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+create_autocmd("BufEnter", {
+  group = create_autogroup("no_comment_on_enter", { clear = true }),
+  desc = "No longer comments upon newline",
+  callback = function() vim.opt_local.formatoptions:remove { "c", "r", "o" } end,
+})
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+create_autocmd("FocusGained", {
+  group = create_autogroup("auto_file_reload", { clear = true }),
+  desc = "Reloads file when change is detected",
+  command = "checktime",
+})
+
+-- don't auto remove highlight on key press other than hlsearch navigation
+vim.on_key(function() end, vim.api.nvim_get_namespaces()["auto_hlsearch"])
