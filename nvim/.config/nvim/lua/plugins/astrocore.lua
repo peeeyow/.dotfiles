d76bc487041b42a6d42551411983eb1e1edee634
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-05-30 18:51:55 +0800

    feat(nvim): configure options

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -10,16 +10,14 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      large_buf = { size = 1024 * 500, lines = 10000 },
+      autopairs = true,
+      cmp = true,
+      diagnostics_mode = 3,
+      highlighturl = true,
+      notifications = true,
     },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
       virtual_text = false,
       update_in_insert = true,
@@ -35,19 +33,27 @@ return {
         prefix = "",
       },
     },
-    -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+      opt = {
+        fillchars = {
+          horiz = "━",
+          horizup = "┻",
+          horizdown = "┳",
+          vert = "┃",
+          vertleft = "┫",
+          vertright = "┣",
+          verthoriz = "╋",
+        },
+        wrap = true,
+        shortmess = vim.tbl_deep_extend("force", vim.opt.shortmess:get(), { c = true }),
+        nrformats = vim.opt.nrformats + { "alpha", "octal", "hex" },
+        whichwrap = vim.opt.whichwrap + "<>[]hl",
+        foldlevel = 99,
+        foldlevelstart = -1,
       },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
+      g = {
+        resession_enabled = true,
+        markdown_recommended_style = false,
       },
     },
     -- Mappings can be configured through AstroCore as well.
