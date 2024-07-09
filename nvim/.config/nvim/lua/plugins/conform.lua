d603ac879ab366123bfec4222525884dbf6889c5
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-07-09 12:23:50 +0800

    feat(nvim): increase conform timeout

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -10,6 +10,7 @@ end
 return {
   "stevearc/conform.nvim",
   opts = {
+    format_on_save = { timeout_ms = 1000 },
     formatters_by_ft = {
       bib = { "bibtex-tidy" },
       django = { "djlint" },
