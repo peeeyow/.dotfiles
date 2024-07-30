5e3ac499f0ff1217ace3eed918f6c54421b61bfa
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-07-30 21:38:03 +0800

    feat: configure zotero

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,30 @@
 ---@type LazySpec
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
-    local open_with_trouble = function(...) return require("trouble.sources.telescope").open(...) end
-    return require("astrocore").extend_tbl(opts, {
-      defaults = {
-        mappings = {
-          i = { ["<c-t>"] = open_with_trouble },
-          n = { ["<c-t>"] = open_with_trouble },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<Leader>fz", "<Cmd>Telescope zotero<CR>", desc = "Find zotero references" },
+    },
+    dependencies = {
+      {
+        "jmbuhr/telescope-zotero.nvim",
+        dependencies = {
+          { "kkharji/sqlite.lua" },
         },
       },
-    })
-  end,
+    },
+    opts = function(_, opts)
+      local open_with_trouble = function(...) return require("trouble.sources.telescope").open(...) end
+      local telescope = require "telescope"
+      telescope.load_extension "zotero"
+      return require("astrocore").extend_tbl(opts, {
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = open_with_trouble },
+            n = { ["<c-t>"] = open_with_trouble },
+          },
+        },
+      })
+    end,
+  },
 }
