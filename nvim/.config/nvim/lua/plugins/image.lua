98d3fde984edc4b3f286507ec492c4047a839da7
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-08-02 00:28:48 +0800

    fix: hide images on other tmux windows

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -16,5 +16,6 @@ return {
     max_height_window_percentage = math.huge,
     max_width_window_percentage = math.huge,
     window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    tmux_show_only_in_active_window = true,
   },
 }
