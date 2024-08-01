afd6d97e0af431024b2f2348b291e982a63c7165
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-08-01 17:07:13 +0800

    fix: unhide status line

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -39,18 +39,4 @@ return {
     opts.mapping = mapping
     return opts
   end,
-  config = function(_, opts)
-    local cmp = require "cmp"
-    cmp.setup(opts)
-
-    cmp.setup.filetype("quarto", {
-      sources = cmp.config.sources {
-        { name = "otter", priority = 1500 },
-        { name = "nvim_lsp", priority = 1000 },
-        { name = "luasnip", priority = 750 },
-        { name = "buffer", priority = 500 },
-        { name = "path", priority = 250 },
-      },
-    })
-  end,
 }
--- nvim/.config/nvim/lua/plugins/molten.lua
+++ nvim/.config/nvim/lua/plugins/molten.lua
@@ -22,8 +22,8 @@ return {
   init = function()
     vim.g.molten_image_provider = "image.nvim"
     vim.g.molten_output_win_max_height = 20
-    vim.g.molten_virt_text_output = true
-    vim.g.molten_virt_lines_off_by_1 = true
-    vim.g.molten_virt_text_max_lines = 1
+    -- vim.g.molten_virt_text_output = true
+    -- vim.g.molten_virt_lines_off_by_1 = true
+    -- vim.g.molten_virt_text_max_lines = 1
   end,
 }
