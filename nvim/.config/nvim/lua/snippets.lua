87f4122d5f148d51190dba234250e83c94d77126
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-09-05 20:21:44 +0800

    fix: assign proper key to prevent snippet reset

--- nvim/.config/nvim/lua/snippets.lua
+++ nvim/.config/nvim/lua/snippets.lua
@@ -9,7 +9,7 @@ local s = ls.snippet
 local t = ls.text_node
 
 -- R: qq to <-
-ls.add_snippets("r", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_all" })
-ls.add_snippets("markdown", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_all" })
-ls.add_snippets("qmd", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_all" })
-ls.add_snippets("quarto", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_all" })
+ls.add_snippets("r", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_arrow_r" })
+ls.add_snippets("markdown", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_arrow_markdown" })
+ls.add_snippets("qmd", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_arrow_qmd" })
+ls.add_snippets("quarto", { s("qq", { t "<- " }) }, { type = "autosnippets", key = "auto_arrow_quarto" })
