5f1d26a26725c1494125fe426e59fbd07ed948d6
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2025-04-11 19:31:20 +0800

    feat(nvim): allow showing of common insert node while typing

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -8,9 +8,11 @@ return {
     opts = require("astrocore").extend_tbl(opts, {
       enable_autosnippets = true,
       store_selection_keys = "<Tab>",
+      update_events = { "TextChanged", "TextChangedI" },
     })
     require "astronvim.plugins.configs.luasnip"(plugin, opts)
     require("luasnip.loaders.from_vscode").lazy_load { paths = "./snippets" }
+    require("luasnip.loaders.from_lua").lazy_load { paths = "./lua/snippets" }
     require("luasnip").filetype_extend("htmldjango", { "djangohtml" })
   end,
 }
