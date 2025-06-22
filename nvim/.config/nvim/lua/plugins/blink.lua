984aca46938a2a58d5520cb7f4789726a0271a5f
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2025-06-22 19:03:14 +0800

    feat: configure blink.nvim

---
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<Tab>"] = {},
+      ["<S-Tab>"] = {},
+    },
+    completion = {
+      list = { selection = { preselect = false, auto_insert = false } },
+    },
+  },
+}
