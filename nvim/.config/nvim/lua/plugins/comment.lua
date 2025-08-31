d06027418c7e3f03b14ff8ba43f63a60129803b8
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2025-08-31 20:06:30 +0800

    fix: move back to comment.nvim

---
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,52 @@
+---@type LazySpec
+return {
+  "numToStr/Comment.nvim",
+  enabled = true,
+  keys = {
+    {
+      "<C-_>",
+      function() require("Comment.api").toggle.linewise.current() end,
+      desc = "Line comment",
+    },
+    {
+      "<C-/>",
+      function() require("Comment.api").toggle.linewise.current() end,
+      desc = "Line comment",
+    },
+    {
+      [[<C-\>]],
+      function() require("Comment.api").toggle.blockwise.current() end,
+      desc = "Block comment",
+    },
+    {
+      "<C-_>",
+      function()
+        local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+        vim.api.nvim_feedkeys(esc, "nx", false)
+        require("Comment.api").toggle.linewise(vim.fn.visualmode())
+      end,
+      mode = { "x" },
+      desc = "Visual Line comment",
+    },
+    {
+      "<C-/>",
+      function()
+        local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+        vim.api.nvim_feedkeys(esc, "nx", false)
+        require("Comment.api").toggle.linewise(vim.fn.visualmode())
+      end,
+      mode = { "x" },
+      desc = "Visual Line comment",
+    },
+    {
+      [[<C-\>]],
+      function()
+        local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+        vim.api.nvim_feedkeys(esc, "nx", false)
+        require("Comment.api").toggle.blockwise(vim.fn.visualmode())
+      end,
+      mode = { "x" },
+      desc = "Visual Block comment",
+    },
+  },
+}
