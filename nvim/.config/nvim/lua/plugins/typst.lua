7586ec5b393b929b48df6215e24720f515debdb8
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2026-07-25 18:56:35 +0800

    fix: migrate to maintained version of yaml-companion.nvim

---
+++ nvim/.config/nvim/lua/plugins/typst.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+  "chomosuke/typst-preview.nvim",
+  version = "1.*",
+  opts = {
+    extra_args = { "--verbose" },
+  },
+}
--- nvim/.config/nvim/lua/plugins/yaml-companion.lua
+++ nvim/.config/nvim/lua/plugins/yaml-companion.lua
@@ -1,23 +1,10 @@
 ---@type LazySpec
 return {
-  "someone-stole-my-name/yaml-companion.nvim",
+  "mosheavni/yaml-companion.nvim",
   ft = { "yaml" },
-  opts = {
-    builtin_matchers = {
-      kubernetes = { enabled = true },
-    },
-  },
-  dependencies = {
-    { "neovim/nvim-lspconfig" },
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim" },
-  },
   config = function(_, opts)
-    local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-    local astrolsp_opts = (astrolsp_avail and astrolsp.lsp_opts "rust_analyzer") or {}
     local cfg = require("yaml-companion").setup(opts)
-    cfg = require("astrocore").extend_tbl(astrolsp_opts, cfg)
-    require("lspconfig")["yamlls"].setup(cfg)
-    require("telescope").load_extension "yaml_schema"
+    vim.lsp.config("yamlls", cfg)
+    vim.lsp.enable "yamlls"
   end,
 }
