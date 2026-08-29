7db34b912733d2b39bbc3fb683c8144ef45861c0
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2026-08-29 17:46:29 +0800

    feat(nvim): monkey patch zotcite for easier obsidian vault usage

--- nvim/.config/nvim/lua/plugins/zotcite.lua
+++ nvim/.config/nvim/lua/plugins/zotcite.lua
@@ -7,4 +7,22 @@ return {
   opts = {
     key_type = "better-bibtex",
   },
+  config = function(_, opts)
+    require("zotcite").setup(opts)
+
+    -- HACK: Monkey Patch zotcite yaml field getter.
+    -- This makes zotcite.get.yaml_field to return default
+    -- obsidian vault bib file if buffer is inside the vault.
+    local vault = vim.fn.expand "~/obsidian/main-vault/"
+    local bibliography = vault .. "references/zotcite.bib"
+    local get = require "zotcite.get"
+    local yaml_field = get.yaml_field
+    get.yaml_field = function(field, bufnr)
+      local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":p")
+      if field == "bibliography" and vim.bo[bufnr].filetype == "markdown" and vim.startswith(path, vault) then
+        return bibliography
+      end
+      return yaml_field(field, bufnr)
+    end
+  end,
 }
